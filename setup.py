e54d5df4d4ee0da23ea79b194717a564ddf5a16b
Aron Fyodor Asor <aronasorman@gmail.com>
2016-12-13 08:47:45 -0800

    Flesh out setup.py and __init__.py for pip to install

---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import pressurecooker
+from setuptools import setup, find_packages
+
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+requirements = [
+    "ffmpy>=0.2.2",
+]
+
+test_requirements = [
+    # TODO: put package test requirements here
+]
+
+setup(
+    name='pressurecooker',
+    version=pressurecooker.__version__,
+    description="A collection of utilities for media processing.",
+    long_description=readme + '\n\n',
+    author="Learning Equality",
+    author_email='dev@learningequality.org',
+    url='https://github.com/learningequality/pressurecooker',
+    packages=find_packages(),
+    package_dir={'presurecooker':
+                 'presurecooker'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="MIT license",
+    zip_safe=False,
+    keywords=['media', 'mediaprocessing', 'video', 'compression', 'thumbnails', 'pressurecooker'],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
