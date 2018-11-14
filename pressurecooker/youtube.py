cf0a9f9fbbd97cf3085368cc9798a7ce17bb7c96
kollivier <kevino@theolliviers.com>
2018-11-14 11:45:47 -0800

    Add subtitle info retrieval and license checking into YouTube module. Also create a class to encasulate functionality.

--- pressurecooker/youtube.py
+++ pressurecooker/youtube.py
@@ -1,18 +1,51 @@
+import sys
+
 import youtube_dl
 
 
 def get_youtube_info(youtube_url):
-    """
-    This function takes a link to a YouTube URL, then returns a dictionary object with info about the video.
+    resource = YouTubeResource(youtube_url)
+    return resource.get_resource_info()
 
-    :param youtube_url: URL of a YouTube video, channel or playlist to retrieve information on.
-    :return: A dictionary object containing information about the channel, playlist or video.
-    """
 
-    client = youtube_dl.YoutubeDL(dict(verbose=True, no_warnings=True))
-    results = client.extract_info(youtube_url, download=False, process=True)
+class YouTubeResource:
+    def __init__(self, url):
+        self.url = url
+        self.resource_info = {}
+        self.subtitles = {}
 
-    def add_to_tree(results):
+        self.get_resource_info()
+
+    def get_resource_info(self, refresh=False):
+        """
+        This method checks the YouTube URL, then returns a dictionary object with info about the video(s) in it.
+
+        :return: A dictionary object containing information about the channel, playlist or video.
+        """
+
+        if len(self.resource_info) > 0 and not refresh:
+            return self.resource_info
+
+        client = youtube_dl.YoutubeDL(dict(verbose=True, no_warnings=True))
+        results = client.extract_info(self.url, download=False, process=True)
+
+        keys = list(results.keys())
+        keys.sort()
+        self.resource_info = self._format_for_ricecooker(results)
+
+        return self.resource_info
+
+    def get_resource_subtitles(self):
+        """
+        Retrieves the subtitles for the video(s) represented by this resource.
+
+        :return:
+        """
+        client = youtube_dl.YoutubeDL(dict(verbose=True, no_warnings=True, writesubtitles=True, allsubtitles=True))
+        results = client.extract_info(self.url, download=False, process=True)
+        return results
+
+    def _format_for_ricecooker(self, results):
         leaf = {}
 
         # dict mapping of field name and default value when not found.
@@ -41,17 +74,49 @@ def get_youtube_info(youtube_url):
             else:
                 leaf[info_name] = extracted_fields[field_name]
 
-        # print("Results = {}".format(results))
         if 'entries' in results:
             leaf['children'] = []
             for entry in results['entries']:
                 if entry is not None:
-                    leaf['children'].append(add_to_tree(entry))
+                    leaf['children'].append(self._format_for_ricecooker(entry))
+                else:
+                    print("Entry is none?")
 
         return leaf
 
-    keys = list(results.keys())
-    keys.sort()
-    tree = add_to_tree(results)
+    def check_for_content_issues(self, filter=False):
+        output_video_info = self.resource_info
+        videos_with_warnings = []
+        if filter:
+            output_video_info['children'] = []
 
-    return tree
+        for video in self.resource_info['children']:
+            warnings = []
+            if not video['license']:
+                warnings.append('no_license_specified')
+            elif video['license'].find("Creative Commons") == -1:
+                warnings.append('closed_license')
+
+            if len(warnings) > 0:
+                videos_with_warnings.append({'video': video, 'warnings': warnings})
+            elif filter:
+                output_video_info['children'].append(video)
+
+        return videos_with_warnings, output_video_info
+
+
+if __name__ == "__main__":
+    check = False
+    if "--check" in sys.argv:
+        sys.argv.remove("--check")
+        check = True
+
+    url = sys.argv[1]
+    yt_resource = YouTubeResource(url)
+    info = yt_resource.get_resource_info()
+    if check:
+        warnings, out_info = yt_resource.check_for_content_issues()
+        for warning in warnings:
+            print('Video {} has the following issues: {}'.format(warning['video']['title'], warning['warnings']))
+    else:
+        print(info)
--- tests/test_youtube.py
+++ tests/test_youtube.py
@@ -1,14 +1,56 @@
 from pressurecooker import youtube
 
-def test_get_youtube_info():
+trees = {}
+yt_resources = {}
 
-    tree = youtube.get_youtube_info('https://www.youtube.com/playlist?list=PLHSER0n5wjJybb22EtsajzufpozEo5vLE')
+cc_playlist = 'https://www.youtube.com/playlist?list=PL7m903CwFUgntbjkVMwts89fZq0INCtVS'
+non_cc_playlist = 'https://www.youtube.com/playlist?list=PLBO8M-O_dTPE51ymDUgilf8DclGAEg9_A'
+subtitles_video = 'https://www.youtube.com/watch?v=6uXAbJQoZlE'
+
+
+def get_yt_resource(url):
+    global yt_resources
+    if not url in yt_resources:
+        yt_resources[url] = youtube.YouTubeResource(url)
+
+    return yt_resources[url]
+
+
+def test_get_youtube_info():
+    yt_resource = get_yt_resource(non_cc_playlist)
+    tree = yt_resource.get_resource_info()
     assert tree['id']
     assert tree['kind']
     assert tree['title']
-    assert len(tree['children']) == 22
+    assert len(tree['children']) == 4
 
     for video in tree['children']:
-        assert tree['id']
-        assert tree['kind']
-        assert tree['title']
+        assert video['id']
+        assert video['kind']
+        assert video['title']
+
+
+def test_warnings_no_license():
+    yt_resource = get_yt_resource(non_cc_playlist)
+    issues, output_info = yt_resource.check_for_content_issues()
+
+    assert len(issues) == 4
+    for issue in issues:
+        assert 'no_license_specified' in issue['warnings']
+
+
+def test_cc_no_warnings():
+    yt_resource = get_yt_resource(cc_playlist)
+    issues, output_info = yt_resource.check_for_content_issues()
+
+    # there is one video in this playlist that is not cc-licensed
+    assert len(issues) == 1
+    for issue in issues:
+        assert 'no_license_specified' in issue['warnings']
+
+
+def test_get_subtitles():
+    yt_resource = get_yt_resource(subtitles_video)
+    info = yt_resource.get_resource_subtitles()
+    assert len(info['subtitles']) == 1
+    assert 'ru' in info['subtitles']
