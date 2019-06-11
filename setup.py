383f46286ab71547b315af6f25a75d47f181b488
Blaine Jester <blainesworld@gmail.com>
2019-06-11 13:33:40 -0700

    Bump youtube-dl to latest

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ requirements = [
     "matplotlib==2.2.3",    # the last with py27 support
     "numpy==1.15.4",        # pinned to avoid surprizes
     "Pillow==5.4.1",        # pinned to avoid surprizes
-    "youtube-dl>=2018.11.7",
+    "youtube-dl>=2019.6.8",
     "pdf2image>=1.4.0",
     "le-pycaption>=2.0.0a1",
 ]
