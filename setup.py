3298858bc500c8aa24967c840c4f3c994a47f393
Ivan Savov <ivan.savov@gmail.com>
2019-11-19 10:51:30 -0500

    update youtube-dl to 2019.11.5

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ requirements = [
     "matplotlib==2.2.3",    # the last with py27 support
     "numpy==1.15.4",        # pinned to avoid surprizes
     "Pillow==5.4.1",        # pinned to avoid surprizes
-    "youtube-dl>=2019.9.1",
+    "youtube-dl>=2019.11.5",
     "pdf2image>=1.4.0",     # needs system dependency poppler lib
     "le-pycaption>=2.0.0a3",
     "EbookLib>=0.17.1",
