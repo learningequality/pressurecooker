81070d9cfcc18467d4d5f4ef536fe81a657bf4d5
Ivan Savov <ivan.savov@gmail.com>
2019-11-19 10:17:59 -0500

    fix one of the youtube test

--- tests/test_youtube.py
+++ tests/test_youtube.py
@@ -96,10 +96,10 @@ def test_download_youtube_playlist():
 def test_get_subtitles():
     yt_resource = get_yt_resource(subtitles_video)
     info = yt_resource.get_resource_subtitles()
-    assert len(info['subtitles']) == 2
+    assert len(info['subtitles']) == 3  # brittle; can change if subs get added
     assert 'ru' in info['subtitles']
     assert 'en' in info['subtitles']
-
+    assert 'zh-CN' in info['subtitles']
 
 def test_non_youtube_url_error():
     url = 'https://vimeo.com/238190750'
