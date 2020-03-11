bbb0efa1300688ca8eb43b446901542a64111a27
Ivan Savov <ivan.savov@gmail.com>
2020-03-11 18:50:26 -0400

    Updated proxy tests

--- tests/test_proxy.py
+++ tests/test_proxy.py
@@ -9,45 +9,43 @@ from pressurecooker import proxy
 from pressurecooker.youtube import YouTubeResource
 
 
+YOUTUBE_TEST_VIDEO = 'https://www.youtube.com/watch?v=C0DPdy98e4c'
+YOUTUBE_TEST_PLAYLIST = 'https://www.youtube.com/playlist?list=PL472BC6F4F2C3ABEF'
+
+
+
 # This test takes a few minutes, but is very useful for checking that the proxy is not being ignored,
 # so mark it to run when the PYTEST_RUN_SLOW env var is set.
 @pytest.mark.skipif(not 'PYTEST_RUN_SLOW' in os.environ, reason="This test takes several minutes to complete.")
-def test_bad_proxies_get_banned():
-    # create some fake proxies, then check that they get banned.
-    proxy.PROXY_LIST = [
+def test_bad_proxies_get_banned(tmp_path):
+    # create some fake proxies...
+    FAKE_PROXIES = [
         '123.123.123.123:1234',
-        '142.123.432.234:123345',
-        '156.245.233.321:1232342',
-        '444.444.444.444:445565',
-        '555.555.555.555:5454'
+        '142.123.1.234:123345',
+        '156.245.233.211:12323',
     ]
+    # initialize PROXY_LIST to known-bad proxies to check that they get banned
+    proxy.PROXY_LIST = FAKE_PROXIES
 
-    temp_dir = tempfile.mkdtemp()
-    video = YouTubeResource('https://www.youtube.com/watch?v=DLzxrzFCyOs')
-    video.download(temp_dir)
+    video = YouTubeResource(YOUTUBE_TEST_VIDEO)
+    video.download(tmp_path)
 
-    temp_files = os.listdir(temp_dir)
-    shutil.rmtree(temp_dir)
-    # if the file downloaded, that's a sign the proxies weren't used.
-    assert temp_files == ['Watch']
-    assert sorted(proxy.BROKEN_PROXIES) == sorted(proxy.PROXY_LIST)
+    # Fake proxies should get added to BROKEN_PROXIES
+    assert set(FAKE_PROXIES).issubset(set(proxy.BROKEN_PROXIES))
 
 
 @pytest.mark.skipif(not 'PYTEST_RUN_SLOW' in os.environ, reason="This test can take several minutes to complete.")
-def test_proxy_download():
+def test_proxy_download(tmp_path):
     proxy.get_proxies(refresh=True)
     assert len(proxy.PROXY_LIST) > 1
 
-    temp_dir = tempfile.mkdtemp()
-    video = YouTubeResource('https://www.youtube.com/watch?v=DLzxrzFCyOs')
-    video.download(temp_dir)
+    video = YouTubeResource(YOUTUBE_TEST_VIDEO)
+    video.download(tmp_path)
 
-    temp_files = os.listdir(os.path.join(temp_dir, 'Watch'))
-    shutil.rmtree(temp_dir)
-    # if the file downloaded, that's a sign the proxies weren't used.
+    temp_files = os.listdir(os.path.join(tmp_path, 'Watch'))
     has_video = False
     for afile in temp_files:
         if afile.endswith('.mp4'):
             has_video = True
 
-    assert has_video
+    assert has_video, 'Video file not found'
