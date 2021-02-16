f7c18842c8bea40f021534091ec7de0eb362f0b0
sairina <sairina.merinotsui@gmail.com>
2021-02-15 19:59:43 -0800

    Add low-res webm test

--- tests/test_videos.py
+++ tests/test_videos.py
@@ -21,7 +21,6 @@ else:
     requests_cache.install_cache("video_cache")
 
 
-
 # FIXTURES
 ################################################################################
 
@@ -52,11 +51,27 @@ def high_res_video():
         f.flush()
     return f            # returns a temporary file with a closed file descriptor
 
+
+@pytest.fixture
+def low_res_video_webm():
+    with TempFile(suffix='.webm') as f:
+        resp = requests.get(
+            "https://mirrors.creativecommons.org/movingimages/"
+            "webm/CreativeCommonsPlusCommercial_240p.webm",
+            stream=True
+        )
+        for chunk in resp.iter_content(chunk_size=1048576):
+            f.write(chunk)
+        f.flush()
+    return f            # returns a temporary file with a closed file descriptor
+
+
 @pytest.fixture
 def high_res_video_webm():
     with TempFile(suffix='.webm') as f:
         resp = requests.get(
-            "https://mirrors.creativecommons.org/movingimages/webm/CreativeCommonsPlusCommercial_720p.webm",
+            "https://mirrors.creativecommons.org/movingimages/"
+            "webm/CreativeCommonsPlusCommercial_720p.webm",
             stream=True
         )
         for chunk in resp.iter_content(chunk_size=1048576):
@@ -78,6 +93,7 @@ def low_res_ogv_video():
             f.write(chunk)
         f.flush()
     return f            # returns a temporary file with a closed file descriptor
+
 
 @pytest.fixture
 def high_res_mov_video():
@@ -102,8 +118,6 @@ def bad_video():
     return f            # returns a temporary file with a closed file descriptor
 
 
-
-
 # TESTS
 ################################################################################
 
@@ -123,17 +137,24 @@ class Test_check_video_resolution:
         preset = videos.guess_video_preset_by_resolution(high_res_video.name)
         assert preset == format_presets.VIDEO_HIGH_RES
 
+    def test_detects_low_res_videos_webm(self, low_res_video_webm):
+        preset = videos.guess_video_preset_by_resolution(
+            low_res_video_webm.name)
+        assert preset == format_presets.VIDEO_LOW_RES
+
     def test_detects_high_res_videos_webm(self, high_res_video_webm):
-        preset = videos.guess_video_preset_by_resolution(high_res_video_webm.name)
+        preset = videos.guess_video_preset_by_resolution(
+            high_res_video_webm.name)
         assert preset == format_presets.VIDEO_HIGH_RES
-
-
 
 
 def get_resolution(videopath):
     """Helper function to get resolution of video at videopath."""
-    result = subprocess.check_output(['ffprobe', '-v', 'error', '-print_format', 'json', '-show_entries',
-                                      'stream=width,height', '-of', 'default=noprint_wrappers=1', str(videopath)])
+    result = subprocess.check_output(['ffprobe', '-v', 'error',
+                                      '-print_format', 'json', '-show_entries',
+                                      'stream=width,height', '-of',
+                                      'default=noprint_wrappers=1',
+                                      str(videopath)])
     pattern = re.compile('width=([0-9]*)[^height]+height=([0-9]*)')
     m = pattern.search(str(result))
     width, height = int(m.group(1)), int(m.group(2))
@@ -144,13 +165,15 @@ class Test_compress_video:
 
     def test_compression_works(self, high_res_video):
         with TempFile(suffix=".mp4") as vout:
-            videos.compress_video(high_res_video.name, vout.name, overwrite=True)
+            videos.compress_video(high_res_video.name,
+                                  vout.name, overwrite=True)
             width, height = get_resolution(vout.name)
-            assert height == 480, 'should compress to 480 v resolution by defualt'
+            assert height == 480, 'should compress to 480 v resolution by default'
 
     def test_compression_max_width(self, high_res_video):
         with TempFile(suffix=".mp4") as vout:
-            videos.compress_video(high_res_video.name, vout.name, overwrite=True, max_width=120)
+            videos.compress_video(high_res_video.name,
+                                  vout.name, overwrite=True, max_width=120)
             width, height = get_resolution(vout.name)
             assert width == 120, 'should be 120 h resolution since max_width set'
 
@@ -159,38 +182,40 @@ class Test_compress_video:
         regression test for: https://github.com/learningequality/pressurecooker/issues/11
         """
         with TempFile(suffix=".mp4") as vout:
-            videos.compress_video(high_res_video.name, vout.name, overwrite=True, max_width=121)
+            videos.compress_video(high_res_video.name,
+                                  vout.name, overwrite=True, max_width=121)
             width, height = get_resolution(vout.name)
             assert width == 120, 'should round down to 120 h resolution when max_width=121 set'
 
     def test_compression_max_height(self, high_res_video):
         with TempFile(suffix=".mp4") as vout:
-            videos.compress_video(high_res_video.name, vout.name, overwrite=True, max_height=140)
+            videos.compress_video(high_res_video.name,
+                                  vout.name, overwrite=True, max_height=140)
             width, height = get_resolution(vout.name)
             assert height == 140, 'should be 140 v resolution since max_height set'
 
     def test_raises_for_bad_file(self, bad_video):
         with TempFile(suffix=".mp4") as vout:
             with pytest.raises(videos.VideoCompressionError):
-                videos.compress_video(bad_video.name, vout.name, overwrite=True)
-
+                videos.compress_video(
+                    bad_video.name, vout.name, overwrite=True)
 
 
 class Test_convert_video:
 
     def test_convert_mov_works(self, high_res_mov_video):
         with TempFile(suffix=".mp4") as vout:
-            videos.compress_video(high_res_mov_video.name, vout.name, overwrite=True)
+            videos.compress_video(high_res_mov_video.name,
+                                  vout.name, overwrite=True)
             width, height = get_resolution(vout.name)
             assert height == 480, 'should convert .ogv to .mp4 and set 480 v res'
 
     def test_convert_and_resize_ogv_works(self, low_res_ogv_video):
         with TempFile(suffix=".mp4") as vout:
-            videos.compress_video(low_res_ogv_video.name, vout.name, overwrite=True, max_height=200)
+            videos.compress_video(low_res_ogv_video.name,
+                                  vout.name, overwrite=True, max_height=200)
             width, height = get_resolution(vout.name)
             assert height == 200, 'should convert .ogv to .mp4 and set 200 v res'
-
-
 
 
 # Helper class for cross-platform temporary files
@@ -203,6 +228,7 @@ def remove_temp_file(*args, **kwargs):
     except FileNotFoundError:
         pass
     assert not os.path.exists(filename)
+
 
 class TempFile(object):
     """
@@ -219,10 +245,10 @@ class TempFile(object):
 
     def __enter__(self):
         # create a temporary file as per usual, but set it up to be deleted once we're done
-        self.f = tempfile.NamedTemporaryFile(*self.args, delete=False, **self.kwargs)
+        self.f = tempfile.NamedTemporaryFile(
+            *self.args, delete=False, **self.kwargs)
         atexit.register(remove_temp_file, self.f.name)
         return self.f
 
     def __exit__(self, _type, value, traceback):
         self.f.close()
-
